db1e21ee166296aafc7c82093b9c7b2907b5f6d3
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-03-20 22:21:35 -0500

    integrated graces spinners into posts

--- public/js/map.js
+++ public/js/map.js
@@ -15,16 +15,17 @@ var popup = L.popup();
 
 var pin = false
 
-var redMarker = L.AwesomeMarkers.icon({icon: 'spinner', prefix: 'fa', markerColor: 'red', spin: true});
+var redMarkerSpin = L.AwesomeMarkers.icon({icon: 'spinner', prefix: 'fa', markerColor: 'red', spin: true});
+var redMarker = L.AwesomeMarkers.icon({icon: 'comment', prefix: 'fa', markerColor: 'red', spin: false});
 
-L.marker([41.8914, -87.6377], { icon: redMarker }).addTo(map);
+// L.marker([41.8914, -87.6377], { icon: redMarker }).addTo(map);
 
 var coffeeMarker = L.AwesomeMarkers.icon({ icon: 'comment', prefix: 'fa', markerColor: 'darkpurple', iconColor: '#f28f82'});
 
-L.marker([41.89, -87.63], { icon: coffeeMarker }).addTo(map);
-
-L.marker([41.8924, -87.6397], { icon: L.AwesomeMarkers.icon({ icon: 'shopping-cart', prefix: 'fa', markerColor: 'blue', iconColor: 'black' }) }).addTo(map);
-L.marker([41.8934, -87.6367], { icon: L.AwesomeMarkers.icon({ icon: 'info', prefix: 'fa', markerColor: 'orange' }) }).addTo(map);
+// L.marker([41.89, -87.63], { icon: coffeeMarker }).addTo(map);
+//
+// L.marker([41.8924, -87.6397], { icon: L.AwesomeMarkers.icon({ icon: 'shopping-cart', prefix: 'fa', markerColor: 'blue', iconColor: 'black' }) }).addTo(map);
+// L.marker([41.8934, -87.6367], { icon: L.AwesomeMarkers.icon({ icon: 'info', prefix: 'fa', markerColor: 'orange' }) }).addTo(map);
 
 
 
@@ -33,6 +34,7 @@ function addMarker(e) {
   if (!pin) {
     var newMarker = new L.marker(e.latlng, {
       draggable: true,
+      icon: redMarkerSpin
     }).addTo(map)
     var position = newMarker.getLatLng();
     console.log("new pin pos:" + position);
@@ -88,21 +90,38 @@ map.on('dblclick', addMarker);
 
 // Populate all postSubject
 function populateMap() {
-  $.get("/api/posts", function(data) {
-    console.log(data)
 
-    for (var y = 0; y<data.length; y++){
-      var lat = data[y].latitude
-      var lon = data[y].longitude
-      var marker = L.marker([lat, lon]).addTo(map);
-      var popupBox = document.createElement('div');
-      $(popupBox).append(`<p>Subject:${data[y].subject}</p>`)
-      $(popupBox).append(`<p>Text:${data[y].text}</p>`)
-      $(popupBox).append(`<p>User_Id:${data[y].user_id}</p>`)
-      marker.bindPopup(popupBox)
+  $.get("/api/user_data").then(function(data1) {
+    currentUserId = ''
+    console.log(data1.id)
+    if (data1.id){
+      currentUserId = data1.id
     }
+    $.get("/api/posts", function(data) {
 
-  });
+      for (var y = 0; y<data.length; y++){
+        var lat = data[y].latitude
+        var lon = data[y].longitude
+
+        console.log(data[y].user_id + " " + currentUserId)
+        if (data[y].user_id - currentUserId ===0){
+          console.log('yes')
+          var marker = L.marker([lat, lon], {icon:redMarker}).addTo(map);
+        }else{
+          console.log('nope')
+          var marker = L.marker([lat, lon], {icon:coffeeMarker}).addTo(map);
+        }
+        var popupBox = document.createElement('div');
+        $(popupBox).append(`<p>Subject:${data[y].subject}</p>`)
+        $(popupBox).append(`<p>Text:${data[y].text}</p>`)
+        $(popupBox).append(`<p>User_Id:${data[y].user_id}</p>`)
+        marker.bindPopup(popupBox)
+      }
+
+    });
+
+  })
+
 }
 
 
