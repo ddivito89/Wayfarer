1085e4e3a67dd169e80263d0f93977e7f2efecf0
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-03-21 23:58:52 -0500

    added socket listeners

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+//socket io requirements
+const socketIO = require('socket.io');
+const http = require('http');
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -11,28 +15,27 @@ var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app, io);
 
-//Set Handlebars
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 
 
 // Starts the server to begin listening
 db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
+  server.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
+});
