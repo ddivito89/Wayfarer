2f8addb8b4e5cb64958dd48df8f81028ab14016c
ddivito89 <daviddivito@dhcp-10-102-194-38.wireless.northwestern.private>
2018-03-23 20:12:42 -0500

    fixxed

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true
       }
     },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
