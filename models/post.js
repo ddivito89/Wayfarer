15990396eeee30ca1021c8f9e0c7487d613fcb59
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-03-24 00:55:32 -0500

    comments added

--- models/post.js
+++ models/post.js
@@ -5,14 +5,14 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     text: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     post_img: {
@@ -26,42 +26,42 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     city: {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     country: {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     latitude: {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     longitude: {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     },
     user_id: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 45]
+        len: [1, 250]
       }
     }
   }, {
